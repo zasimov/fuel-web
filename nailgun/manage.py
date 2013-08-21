3412898ec6340de87cd03265edab5081f7c058dc
Nikolay Markov <nmarkov@mirantis.com>
2013-08-21 15:39:57 +0400

    manage.py refactoring

--- nailgun/manage.py
+++ nailgun/manage.py
@@ -20,147 +20,263 @@ import sys
 import argparse
 import code
 import web
+import logging
+
+
+def syncdb_action(params):
+    """
+    sync application database
+    """
+    from nailgun.logger import logger
+    from nailgun.db import syncdb
+    logger.info("Syncing database...")
+    syncdb()
+    logger.info("Done")
+
+def dropdb_action(params):
+    """
+    drop application database
+    """
+    from nailgun.logger import logger
+    from nailgun.db import dropdb
+    logger.info("Dropping database...")
+    dropdb()
+    logger.info("Done")
+
+def test_action(params):
+    """
+    run unit tests
+    """
+    from nailgun.logger import logger
+    from nailgun.unit_test import TestRunner
+    logger.info("Running tests...")
+    TestRunner.run()
+    logger.info("Done")
+
+def loaddata_action(params):
+    """
+    load data from fixture
+    """
+    from nailgun.logger import logger
+    from nailgun.fixtures import fixman
+    logger.info("Uploading fixture...")
+    with open(params.fixture, "r") as fileobj:
+        fixman.upload_fixture(fileobj)
+    logger.info("Done")
+
+def dumpdata_action(params):
+    """
+    dump data to JSON
+    """
+    logging.disable(logging.WARNING)
+    from nailgun.fixtures import fixman
+    fixman.dump_fixture(params.model)
+
+def loaddefault_action(params):
+    """
+    load data from default fixtures
+    (settings.FIXTURES_TO_IPLOAD)
+    """
+    from nailgun.logger import logger
+    from nailgun.fixtures import fixman
+    logger.info("Uploading fixture...")
+    fixman.upload_fixtures()
+    logger.info("Done")
+
+def run_action(params):
+    """
+    run application locally
+    """
+    from nailgun.settings import settings
+    from nailgun.wsgi import appstart
+    settings.update({
+        'LISTEN_PORT': int(params.port),
+        'LISTEN_ADDRESS': params.address,
+    })
+    for attr in ['FAKE_TASKS', 'FAKE_TASKS_TICK_COUNT',
+                 'FAKE_TASKS_TICK_INTERVAL', 'FAKE_TASKS_AMQP']:
+        param = getattr(params, attr.lower())
+        if param is not None:
+            settings.update({attr: param})
+    if params.config_file:
+        settings.update_from_file(params.config_file)
+    appstart(keepalive=params.keepalive)
+
+def shell_action(params):
+    """
+    open python REPL
+    """
+    from nailgun.settings import settings
+    if params.config_file:
+        settings.update_from_file(params.config_file)
+    try:
+        from IPython import embed
+        embed()
+    except ImportError:
+        code.interact(local={'db': db, 'settings': settings})
+
+def dump_settings_action(params):
+    """
+    dump current settings to YAML
+    """
+    from nailgun.settings import settings
+    sys.stdout.write(settings.dump())
+
 
 if __name__ == "__main__":
+    actions = {
+        "run": {
+            "action": run_action,
+            "args": [
+                {
+                    "args": ["-p", "--port"],
+                    "params": {
+                        "dest": "port",
+                        "action": "store",
+                        "type": str,
+                        "help": "application port",
+                        "default": "8000"
+                    }
+                },
+                {
+                    "args": ["-a", "--address"],
+                    "params": {
+                        "dest": "address",
+                        "action": "store",
+                        "type": str,
+                        "help": "application address",
+                        "default": "0.0.0.0"
+                    }
+                },
+                {
+                    "args": ["-c", "--config"],
+                    "params": {
+                        "dest": "config_file",
+                        "action": "store",
+                        "type": str,
+                        "help": "custom config file",
+                        "default": None
+                    }
+                },
+                {
+                    "args": ["--fake-tasks"],
+                    "params": {
+                        "action": "store_true",
+                        "help": "fake tasks"
+                    }
+                },
+                {
+                    "args": ["--fake-tasks-tick-count"],
+                    "params": {
+                        "action": "store",
+                        "type": int,
+                        "help": "Fake tasks tick count"
+                    }
+                },
+                {
+                    "args": ["--fake-tasks-tick-interval"],
+                    "params": {
+                        "action": "store",
+                        "type": int,
+                        "help": "Fake tasks tick interval"
+                    }
+                },
+                {
+                    "args": ["--fake-tasks-amqp"],
+                    "params": {
+                        "action": "store_true",
+                        "help": "fake tasks with real amqp"
+                    }
+                },
+                {
+                    "args": ["--keepalive"],
+                    "params": {
+                        "action": "store_true",
+                        "help": "run keepalive thread"
+                    }
+                },
+
+            ]
+        },
+        "test": {
+            "action": test_action
+        },
+        "syncdb": {
+            "action": syncdb_action
+        },
+        "dropdb": {
+            "action": dropdb_action
+        },
+        "shell": {
+            "action": shell_action,
+            "args": [
+                {
+                    "args": ["-c", "--config"],
+                    "params": {
+                        "dest": "config_file",
+                        "action": "store",
+                        "type": str,
+                        "help": "custom config file",
+                        "default": None
+                    }
+                },
+            ]
+        },
+        "loaddata": {
+            "action": loaddata_action,
+            "args": [
+                {
+                    "args": ["fixture"],
+                    "params": {
+                        "action": "store",
+                        "help": "json fixture to load"
+                    }
+                },
+            ]
+        },
+        "dumpdata": {
+            "action": dumpdata_action,
+            "args": [
+                {
+                    "args": ["model"],
+                    "params": {
+                        "action": "store",
+                        "help": "model name to dump; "
+                                "underscored name "
+                                "should be used, e.g. "
+                                "network_group for "
+                                "for NetworkGroup model"
+                    }
+                },
+            ]
+        },
+        "loaddefault": {
+            "action": loaddefault_action
+        },
+        "dump_settings": {
+            "action": dump_settings_action
+        }
+    }
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(
         dest="action", help='actions'
     )
-    run_parser = subparsers.add_parser(
-        'run', help='run application locally'
-    )
-    run_parser.add_argument(
-        '-p', '--port', dest='port', action='store', type=str,
-        help='application port', default='8000'
-    )
-    run_parser.add_argument(
-        '-a', '--address', dest='address', action='store', type=str,
-        help='application address', default='0.0.0.0'
-    )
-    run_parser.add_argument(
-        '--fake-tasks', action='store_true', help='fake tasks'
-    )
-    run_parser.add_argument(
-        '--fake-tasks-amqp', action='store_true',
-        help='fake tasks with real AMQP'
-    )
-    run_parser.add_argument(
-        '--keepalive',
-        action='store_true',
-        help='run keep alive thread'
-    )
-    run_parser.add_argument(
-        '-c', '--config', dest='config_file', action='store', type=str,
-        help='custom config file', default=None
-    )
-    run_parser.add_argument(
-        '--fake-tasks-tick-count', action='store', type=int,
-        help='Fake tasks tick count'
-    )
-    run_parser.add_argument(
-        '--fake-tasks-tick-interval', action='store', type=int,
-        help='Fake tasks tick interval in seconds'
-    )
-    test_parser = subparsers.add_parser(
-        'test', help='run unit tests'
-    )
-    syncdb_parser = subparsers.add_parser(
-        'syncdb', help='sync application database'
-    )
-    dropdb_parser = subparsers.add_parser(
-        'dropdb', help='drop application database'
-    )
-    shell_parser = subparsers.add_parser(
-        'shell', help='open python REPL'
-    )
-    shell_parser.add_argument(
-        '-c', '--config', dest='config_file', action='store', type=str,
-        help='custom config file', default=None
-    )
-    loaddata_parser = subparsers.add_parser(
-        'loaddata', help='load data from fixture'
-    )
-    loaddata_parser.add_argument(
-        'fixture', action='store', help='json fixture to load'
-    )
-    dumpdata_parser = subparsers.add_parser(
-        'dumpdata', help='dump models as fixture'
-    )
-    dumpdata_parser.add_argument(
-        'model', action='store', help='model name to dump; underscored name'
-        'should be used, e.g. network_group for NetworkGroup model'
-    )
-    loaddefault_parser = subparsers.add_parser(
-        'loaddefault',
-        help='load data from default fixtures '
-             '(settings.FIXTURES_TO_IPLOAD)'
-    )
-    dump_settings = subparsers.add_parser(
-        'dump_settings', help='dump current settings to YAML'
-    )
+
+    for action, params in actions.iteritems():
+        action_parser = subparsers.add_parser(
+            action, help=params["action"].__doc__
+        )
+        for arg in params.get("args", []):
+            action_parser.add_argument(
+                *arg["args"],
+                **arg["params"]
+            )
+
     params, other_params = parser.parse_known_args()
     sys.argv.pop(1)
 
-    if params.action == "dumpdata":
-        import logging
-        logging.disable(logging.WARNING)
-        from nailgun.fixtures import fixman
-        fixman.dump_fixture(params.model)
+    if not params.action in actions:
+        parser.print_help()
         sys.exit(0)
 
-    from nailgun.logger import logger
-    from nailgun.settings import settings
-
-    if params.action == "syncdb":
-        logger.info("Syncing database...")
-        from nailgun.db import syncdb
-        syncdb()
-        logger.info("Done")
-    elif params.action == "dropdb":
-        logger.info("Dropping database...")
-        from nailgun.db import dropdb
-        dropdb()
-        logger.info("Done")
-    elif params.action == "test":
-        logger.info("Running tests...")
-        from nailgun.unit_test import TestRunner
-        TestRunner.run()
-        logger.info("Done")
-    elif params.action == "loaddata":
-        logger.info("Uploading fixture...")
-        from nailgun.fixtures import fixman
-        with open(params.fixture, "r") as fileobj:
-            fixman.upload_fixture(fileobj)
-        logger.info("Done")
-    elif params.action == "loaddefault":
-        logger.info("Uploading fixture...")
-        from nailgun.fixtures import fixman
-        fixman.upload_fixtures()
-        logger.info("Done")
-    elif params.action == "dump_settings":
-        sys.stdout.write(settings.dump())
-    elif params.action in ("run",):
-        settings.update({
-            'LISTEN_PORT': int(params.port),
-            'LISTEN_ADDRESS': params.address,
-        })
-        for attr in ['FAKE_TASKS', 'FAKE_TASKS_TICK_COUNT',
-                     'FAKE_TASKS_TICK_INTERVAL', 'FAKE_TASKS_AMQP']:
-            param = getattr(params, attr.lower())
-            if param is not None:
-                settings.update({attr: param})
-        if params.config_file:
-            settings.update_from_file(params.config_file)
-        from nailgun.wsgi import appstart
-        appstart(keepalive=params.keepalive)
-    elif params.action == "shell":
-        from nailgun.db import db
-        if params.config_file:
-            settings.update_from_file(params.config_file)
-        try:
-            from IPython import embed
-            embed()
-        except ImportError:
-            code.interact(local={'db': db, 'settings': settings})
-    else:
-        parser.print_help()
+    actions[params.action]["action"](params)
